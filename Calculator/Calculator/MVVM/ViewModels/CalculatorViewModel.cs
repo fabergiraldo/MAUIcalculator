f8c7c43d975d9b0171aa1b58b11f00616c0bfaf5
fabergiraldo <giraldo861@gmail.com>
2023-05-16 22:05:16 -0500

    First view

---
+++ Calculator/Calculator/MVVM/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.MVVM.ViewModels
+{
+    
+    public class CalculatorViewModel
+    {
+        int currentState = 1;
+        string operatorMath;
+        //min video 8:00
+
+        void OnClear(object sender, EventArgs e)
+        {
+        }
+
+        void OnDivide(object sender, EventArgs e)
+        {
+        }
+
+        void OnNumberSelection(object sender, EventArgs e)
+        {
+        }
+
+    }
+
+    
+}
